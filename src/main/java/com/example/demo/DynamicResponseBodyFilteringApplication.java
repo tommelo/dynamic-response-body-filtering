becd53689d44371adab071c2636a376731599a25
tommelo <mail2.tommelo@gmail.com>
2022-08-19 12:45:14 +0200

    add response body filtering demo

---
+++ src/main/java/com/example/demo/DynamicResponseBodyFilteringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicResponseBodyFilteringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicResponseBodyFilteringApplication.class, args);
+    }
+
+}
